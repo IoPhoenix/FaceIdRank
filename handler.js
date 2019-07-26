eeba897ac92d899f23f0cb47fd6fc779fe251e5c
Olga Fomin <olga.fomin26@gmail.com>
2019-07-25 23:15:41 -0700

    Add Access-Control-Allow-Origin header

--- handler.js
+++ handler.js
@@ -1,14 +1,22 @@
 'use strict';
 
+const hearts = [
+  '💕', '💞', '💓', '💗', '💖', '💝'
+];
+
 module.exports.rank = async (event, context, callback) => {
 
-  console.log(event); // Contains incoming request data (e.g., query params, headers and more)
+  const rank = event.queryStringParameters.rank;
+  const rankHeart = hearts[rank > hearts.length ? hearts.length - 1 : rank];
 
   const response = {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      input: rankHeart,
     }, null, 2),
   };
 
