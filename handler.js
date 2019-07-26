fe623dc794b2bbedac5e9ad6f1a1be934c561829
Olga Fomin <olga.fomin26@gmail.com>
2019-07-25 21:55:27 -0700

    Add callback to rank function

--- handler.js
+++ handler.js
@@ -1,13 +1,18 @@
 'use strict';
 
-module.exports.rank = async (event) => {
-  return {
+module.exports.rank = async (event, context, callback) => {
+
+  console.log(event); // Contains incoming request data (e.g., query params, headers and more)
+
+  const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
     }, null, 2),
   };
+
+  callback(null, response);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
